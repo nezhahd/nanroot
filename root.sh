0ae9812d296bc4a8b058867b0eae2eb30d9e3929
scott morrison <102368147+nezhahd@users.noreply.github.com>
2024-06-14 14:13:36 +0800

    Create root.sh

---
+++ root.sh
@@ -0,0 +1,5 @@
+passwd
+sed -i 's/^\s*#\?\s*PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
+sed -i 's/^\s*#\?\s*PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
+rm -rf /etc/ssh/sshd_config.d/* /etc/ssh/ssh_config.d/*
+systemctl restart ssh
