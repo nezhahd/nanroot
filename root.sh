f6b744c87848cbd3aa0e8578a93e4c27f2e50f51
scott morrison <102368147+nezhahd@users.noreply.github.com>
2024-06-14 14:35:35 +0800

    Update root.sh

--- root.sh
+++ root.sh
@@ -1,5 +1,9 @@
+echo "设置你的ROOT密码"
+
 passwd
 sed -i 's/^\s*#\?\s*PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
 sed -i 's/^\s*#\?\s*PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
 rm -rf /etc/ssh/sshd_config.d/* /etc/ssh/ssh_config.d/*
 systemctl restart ssh
+
+echo -e "ROOT登录设置完毕！"
